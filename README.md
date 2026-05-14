4bcaf152d4eef4b19c48df1f4308f881303c9806
José Luis Medina <j.lmg4545@gmail.com>
2026-05-14 17:57:33 -0600

    add dotenv config

---
+++ README.md
@@ -0,0 +1,14 @@
+# Book Tracker
+
+App to keep track every book read through the year.
+
+## Stack
+
+- Node.js / Express
+- PostgreSQL
+- Vanilla JS
+
+## Run locally
+
+npm install
+npm start
--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 import express from "express";
 import pg from "pg";
+import "dotenv/config";
 
 const app = express();
 const port = 3000;
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "Books",
-  password: "JlMg300600",
-  port: 5432,
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
 });
 
 db.connect();
